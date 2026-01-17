1b3754e1a78f70aeaeee67dc0f210b0b876fe94d
Kauan Allyson <kauanallysonrp@gmail.com>
2026-01-17 20:40:21 -0300

    add swagger and deploy properties

--- src/main/java/com/kauanallyson/rifa_hub_api/config/security/SecurityConfig.java
+++ src/main/java/com/kauanallyson/rifa_hub_api/config/security/SecurityConfig.java
@@ -28,6 +28,8 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/swagger-ui/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
